b2a13495362ef323d22d2cea08e5039ff62c822f
Yadnesh Tendolkar <144663742+YadneshTendolkar06@users.noreply.github.com>
2024-12-04 17:02:02 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,13 +6,7 @@ import dotenv from 'dotenv';
 
 const app = express()
 dotenv.config();
-app.use(cors(
-    {
-        origin: ['https://student-management-system-frontend-beryl.vercel.app/'],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-))
+app.use(cors())
 app.use(express.json());
 
 const db = mysql.createConnection({host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE})
